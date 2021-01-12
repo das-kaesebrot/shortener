ee3a74a9b5870a9f085e3f73c63d9cfbdb6ab624
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-01-12 22:28:22 +0100

    Add main, debug and launch config

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include <cgicc/CgiDefs.h> 
+#include <cgicc/Cgicc.h> 
+#include <cgicc/HTTPHTMLHeader.h> 
+#include <cgicc/HTMLClasses.h> 
+
+const std::string ENV[24] = {
+   "COMSPEC", "DOCUMENT_ROOT", "GATEWAY_INTERFACE",   
+   "HTTP_ACCEPT", "HTTP_ACCEPT_ENCODING",             
+   "HTTP_ACCEPT_LANGUAGE", "HTTP_CONNECTION",         
+   "HTTP_HOST", "HTTP_USER_AGENT", "PATH",            
+   "QUERY_STRING", "REMOTE_ADDR", "REMOTE_PORT",      
+   "REQUEST_METHOD", "REQUEST_URI", "SCRIPT_FILENAME",
+   "SCRIPT_NAME", "SERVER_ADDR", "SERVER_ADMIN",      
+   "SERVER_NAME","SERVER_PORT","SERVER_PROTOCOL",     
+   "SERVER_SIGNATURE","SERVER_SOFTWARE" };
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < std::size(ENV); i++)
+    {
+        std::cout << "Env var: " << ENV[i] << "\n";
+        std::cout << "Value: " << getenv(ENV[i].c_str()) << "\n";
+    }   
+    return 0;
+}
