9162dd163a11ebf1d83f43c38a186a17b111440e
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-01-15 22:00:53 +0100

    Add cgicc example

--- main.cpp
+++ main.cpp
@@ -2,30 +2,26 @@
 #include <stdlib.h>
 #include <iterator>
 
-#include "cgicc/Cgicc.h"
-#include "cgicc/HTTPHTMLHeader.h"
-#include "cgicc/HTMLClasses.h"
+#include <cgicc/Cgicc.h>
+#include <cgicc/HTTPHTMLHeader.h>
+#include <cgicc/HTMLClasses.h>
 
 using namespace std;
-
-
-const std::string ENV[] = {
-   "COMSPEC", "DOCUMENT_ROOT", "GATEWAY_INTERFACE",   
-   "HTTP_ACCEPT", "HTTP_ACCEPT_ENCODING",             
-   "HTTP_ACCEPT_LANGUAGE", "HTTP_CONNECTION",         
-   "HTTP_HOST", "HTTP_USER_AGENT", "PATH",            
-   "QUERY_STRING", "REMOTE_ADDR", "REMOTE_PORT",      
-   "REQUEST_METHOD", "REQUEST_URI", "SCRIPT_FILENAME",
-   "SCRIPT_NAME", "SERVER_ADDR", "SERVER_ADMIN",      
-   "SERVER_NAME","SERVER_PORT","SERVER_PROTOCOL",     
-   "SERVER_SIGNATURE","SERVER_SOFTWARE" };
+using namespace cgicc;
 
 int main(int argc, char *argv[])
 {
-    for (int i = 0; i < std::size(ENV); i++)
-    {
-        std::cout << "Env var: " << ENV[i] << "\n";
-        std::cout << "Value: " << getenv(ENV[i].c_str()) << "\n";
-    }   
-    return 0;
+    try {
+      Cgicc cgi;
+
+      // Send HTTP header
+      cout << HTTPHTMLHeader() << endl;
+
+      // Set up the HTML document
+      cout << html() << head(title("cgicc example")) << endl;
+      cout << body("Test") << html() << endl;
+   }
+   catch(exception& e) {
+      // handle any errors - omitted for brevity
+   }
 }
