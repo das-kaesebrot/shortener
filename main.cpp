898848394c744fd4bd6a113ad03a10afecd4971c
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-01-20 01:51:44 +0100

    Add query string output (ugly hack)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <iterator>
+#include <sstream> 
 
 #include <cgicc/Cgicc.h>
 #include <cgicc/HTTPHTMLHeader.h>
@@ -14,18 +15,23 @@ int main(int argc, char *argv[])
    string website_title = "Kaesebrot's URL shortener";
    try {
       Cgicc cgi;
-
-      // get Path Info
       string querystr = cgi.getEnvironment().getQueryString();
+      std::stringstream queryss(querystr);
 
       // Send HTTP header
       cout << HTTPHTMLHeader() << endl;
 
       // Set up the HTML document
       cout << html() << head(title(website_title)) << endl;
-      cout << body("Query string: " + querystr) << html() << endl;
 
-      // switch case
+      // Get query string (I know, it's a hack)
+      string s;
+      getline(queryss, s, '=');
+      getline(queryss, s, '=');
+      
+      // cout << "Found: " << out << endl;
+      cout << body("Query string: " + s) << html() << endl;
+      
    }
    catch(exception& e) {
       cout << HTTPHTMLHeader() << endl;
