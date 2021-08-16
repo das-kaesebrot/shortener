1c94916713fb5ed406ad94b61d4d45f50ae3e8e2
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-08-16 16:53:12 +0200

    Add user data access object

---
+++ src/main/java/eu/ksbrt/shortener/dao/UserDao.java
@@ -0,0 +1,14 @@
+package eu.ksbrt.shortener.dao;
+
+import eu.ksbrt.shortener.model.User;
+
+import java.util.UUID;
+
+public interface UserDao {
+    int insertUser(UUID id, User user);
+
+    default int  addUser(User user) {
+        UUID id = UUID.randomUUID();
+        return insertUser(id, user);
+    }
+}
