f6eba45aad271c105977a20a82beebd579806460
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-08-16 21:14:02 +0200

    Add rest tags

--- src/main/java/eu/ksbrt/shortener/api/UserController.java
+++ src/main/java/eu/ksbrt/shortener/api/UserController.java
@@ -1,10 +1,13 @@
 package eu.ksbrt.shortener.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import eu.ksbrt.shortener.model.User;
 import eu.ksbrt.shortener.service.UserService;
 
+@RestController
 public class UserController {
     
     private final UserService userService;
@@ -14,6 +17,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping
     public void addUser(User user) {
         userService.addUser(user);
     }
