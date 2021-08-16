034a87d7bc7e425558b182ab4f113b240f692efe
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-08-16 16:36:40 +0200

    Init gradle project

---
+++ shortener/src/main/java/eu/ksbrt/shortener/ShortenerApplication.java
@@ -0,0 +1,13 @@
+package eu.ksbrt.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerApplication.class, args);
+	}
+
+}
