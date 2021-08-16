95f9b8bd142ed4680e40771e434bb2be6ecb71a8
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-08-16 21:42:09 +0200

    Fix data source auto config

--- src/main/java/eu/ksbrt/shortener/ShortenerApplication.java
+++ src/main/java/eu/ksbrt/shortener/ShortenerApplication.java
@@ -2,8 +2,10 @@ package eu.ksbrt.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ShortenerApplication {
 
 	public static void main(String[] args) {
