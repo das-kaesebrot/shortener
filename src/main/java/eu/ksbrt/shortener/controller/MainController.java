bc16a8b6ad5a6e8fd8254cc0fcbe4c7863dfaf26
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-09-15 11:18:43 +0200

    fix: getMapping for root

--- src/main/java/eu/ksbrt/shortener/controller/MainController.java
+++ src/main/java/eu/ksbrt/shortener/controller/MainController.java
@@ -1,21 +1,15 @@
 package eu.ksbrt.shortener.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-import eu.ksbrt.shortener.service.*;
-import eu.ksbrt.shortener.model.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	@GetMapping("/")
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
 		model.addAttribute("name", name);
 		return "test";
 	}
