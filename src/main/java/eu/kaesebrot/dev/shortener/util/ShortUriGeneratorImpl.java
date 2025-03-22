a3fb2ce2cdbe36265c33eac338e4f5a1ee80b0ee
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-03-23 00:15:56 +0100

    add very crude short uri generator

--- src/main/java/eu/kaesebrot/dev/shortener/util/ShortUriGeneratorImpl.java
+++ src/main/java/eu/kaesebrot/dev/shortener/util/ShortUriGeneratorImpl.java
@@ -1,9 +1,11 @@
 package eu.kaesebrot.dev.shortener.util;
 
+import java.util.UUID;
+
 public class ShortUriGeneratorImpl implements ShortUriGenerator {
 
     @Override
     public String generate(int length) {
-        return null;
+        return UUID.randomUUID().toString().substring(0, length);
     }
 }
