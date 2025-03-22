fe7c171087f58a66f0e8ca8ff06c3242ebf0694a
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-03-23 00:19:16 +0100

    use hashing to generate unique short uri

--- src/main/java/eu/kaesebrot/dev/shortener/util/ShortUriGeneratorImpl.java
+++ src/main/java/eu/kaesebrot/dev/shortener/util/ShortUriGeneratorImpl.java
@@ -1,11 +1,23 @@
 package eu.kaesebrot.dev.shortener.util;
 
-import java.util.UUID;
+import java.security.SecureRandom;
+import java.util.HexFormat;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class ShortUriGeneratorImpl implements ShortUriGenerator {
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final SecureRandom secureRandom = new SecureRandom();
 
+    // this is terrible
     @Override
     public String generate(int length) {
-        return UUID.randomUUID().toString().substring(0, length);
+        var seed = secureRandom.generateSeed(secureRandom.nextInt(24, 32));
+        var rawKey = HexFormat.of().formatHex(seed);
+
+        var hashedKey = passwordEncoder.encode(rawKey);
+
+        return hashedKey.substring(0, length);
     }
 }
