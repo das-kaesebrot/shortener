2207da008235647243354ea940e09101af9c20bb
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-03-23 20:00:04 +0100

    refactor link controller

--- src/main/java/eu/kaesebrot/dev/shortener/link/LinkController.java
+++ src/main/java/eu/kaesebrot/dev/shortener/link/LinkController.java
@@ -1,40 +1,57 @@
 package eu.kaesebrot.dev.shortener.link;
+import java.io.IOException;
 import java.util.Set;
 import java.util.HashSet;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import eu.kaesebrot.dev.shortener.exceptions.LinkNotFoundException;
+import eu.kaesebrot.dev.shortener.model.Link;
+import eu.kaesebrot.dev.shortener.model.LinkCreation;
+import eu.kaesebrot.dev.shortener.service.LinkRepository;
+import eu.kaesebrot.dev.shortener.util.ShortUriGenerator;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/shortener")
+@Tag(name = "link", description = "The Link API")
 public class LinkController {
     private final LinkRepository _linkRepository;
+    private final ShortUriGenerator _shortUriGenerator;
     
-    LinkController(LinkRepository repository) {
+    LinkController(LinkRepository repository, ShortUriGenerator shortUriGenerator) {
         _linkRepository = repository;
+        _shortUriGenerator = shortUriGenerator;
     }
 
-    // Aggregate root
-    // tag::get-aggregate-root[]
-    @GetMapping("/links")
+    @GetMapping("links")
     Set<Link> getAllLinks() {
         return new HashSet<>(_linkRepository.findAll());
     }
-    // end::get-aggregate-root[]
 
-    @PostMapping("/links")
-    Link createSingleLink(@RequestBody RequestBody requestBody) {
-        _linkRepository.save(
-                new Link(
-                        null, null, null
-                )
-        );
-        return null;
+    @PostMapping("links")
+    Link createSingleLink(LinkCreation linkCreation) {
+        String generatedUri = null;
+        do {
+            generatedUri = _shortUriGenerator.generate(5);
+        } while (_linkRepository.existsByShortUri(generatedUri));
+
+        Link link = new Link(generatedUri, linkCreation.getUrl(), null);
+
+        _linkRepository.save(link);
+
+        return link;
     }
 
-    @GetMapping("/links/{id}")
-    Link getSingleLink(@PathVariable String id) {
+    @GetMapping("links/{id}")
+    Link getSingleLink(@PathVariable Long id) {
         return _linkRepository.findById(id).orElseThrow(() -> new LinkNotFoundException(id));
+    }
+
+    @GetMapping("/redirect/{shortUri}")
+    void redirectShortUri(HttpServletResponse response, @PathVariable String shortUri) throws IOException {
+        var link = _linkRepository.findByShortUri(shortUri).orElseThrow();
+
+        response.sendRedirect(link.getFullURI().toString());
     }
 }
