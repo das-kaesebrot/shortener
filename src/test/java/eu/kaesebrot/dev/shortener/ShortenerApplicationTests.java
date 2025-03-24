8c04fef389db2a82d44f7e179ea14cbe49ce2e89
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-03-24 15:33:11 +0100

    add hex string generator tests

--- src/test/java/eu/kaesebrot/dev/shortener/ShortenerApplicationTests.java
+++ src/test/java/eu/kaesebrot/dev/shortener/ShortenerApplicationTests.java
@@ -1,7 +1,11 @@
 package eu.kaesebrot.dev.shortener;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,9 +18,21 @@ class ShortenerApplicationTests {
     private HexStringGenerator hexStringGenerator;
 
 	@Test
-	void testHexStringLength() {
-		var generatedString = hexStringGenerator.generate(5);
+	@DisplayName("Generate correct length hex String")
+	void givenLength_whenGenerate_thenGenerateHexStringInExpectedLength() {
+		int expectedLength = 5;
+		var generatedString = hexStringGenerator.generate(expectedLength);
 		
-		assertEquals(5, generatedString.length());
+		assertEquals(expectedLength, generatedString.length());
+		assertTrue(Pattern.matches("^[a-f0-9]+$", generatedString));
+	}
+
+	@Test
+	@DisplayName("Generate correct length token")
+	void whenGenerateToken_thenGenerateTokenInExpectedLength() {
+		var generatedString = hexStringGenerator.generateToken();
+		
+		assertEquals(64, generatedString.length());
+		assertTrue(Pattern.matches("^[a-f0-9]+$", generatedString));
 	}
 }
