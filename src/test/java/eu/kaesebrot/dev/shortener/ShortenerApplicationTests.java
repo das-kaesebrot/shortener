f8136dfe73ac1746f608d7a895a457089a065b94
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-03-24 01:28:54 +0100

    initial hex string generator test

--- src/test/java/eu/kaesebrot/dev/shortener/ShortenerApplicationTests.java
+++ src/test/java/eu/kaesebrot/dev/shortener/ShortenerApplicationTests.java
@@ -1,14 +1,22 @@
 package eu.kaesebrot.dev.shortener;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import eu.kaesebrot.dev.shortener.util.ShortUriGenerator;
 
 @SpringBootTest
 class ShortenerApplicationTests {
+    @Autowired
+    private ShortUriGenerator shortUriGenerator;
 
-	/* @Test
-	void contextLoads() {
+	@Test
+	void testHexStringLength() {
+		var generatedString = shortUriGenerator.generate(5);
+		
+		assertEquals(5, generatedString.length());
 	}
-	*/
-
 }
