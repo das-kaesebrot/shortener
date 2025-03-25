be79ededb1db8d12d77361c3b6db6e44edf18ea2
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-03-25 12:28:04 +0100

    add DatabaseProperties

---
+++ src/main/java/eu/kaesebrot/dev/shortener/properties/DatabaseProperties.java
@@ -0,0 +1,53 @@
+package eu.kaesebrot.dev.shortener.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class DatabaseProperties {
+    private String url = "jdbc:sqlite:test.db";
+    private String driverClassName = "org.sqlite.JDBC";
+    private String databasePlatform = "org.hibernate.community.dialect.SQLiteDialect";
+    private String username;
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getDatabasePlatform() {
+        return databasePlatform;
+    }
+
+    public void setDatabasePlatform(String databasePlatform) {
+        this.databasePlatform = databasePlatform;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
