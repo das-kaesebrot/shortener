fc90cd342703d47d7695ff6803490e002d562623
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-07-27 11:03:20 +0200

    feature: return custom userDetailsService, always use BCryptPasswordEncoder

--- src/main/java/eu/kaesebrot/dev/shortener/security/SecurityConfiguration.java
+++ src/main/java/eu/kaesebrot/dev/shortener/security/SecurityConfiguration.java
@@ -1,13 +1,13 @@
 package eu.kaesebrot.dev.shortener.security;
 
+import eu.kaesebrot.dev.shortener.service.AuthUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -15,6 +15,12 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final AuthUserDetailsService _authUserDetailsService;
+
+    public SecurityConfiguration(AuthUserDetailsService authUserDetailsService) {
+        _authUserDetailsService = authUserDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -42,12 +48,12 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+    public UserDetailsService userDetailsService() {
+        return _authUserDetailsService;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
