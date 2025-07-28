d7b01982c0d3606b73da0b5b31a2aaa3e11d2388
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-07-28 21:58:08 +0200

    feature: add pageable getter for all users

--- src/main/java/eu/kaesebrot/dev/shortener/api/UserController.java
+++ src/main/java/eu/kaesebrot/dev/shortener/api/UserController.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +56,11 @@ public class UserController {
     @GetMapping("{id}")
     ShortenerUser getUser(@PathVariable UUID id) {
         return userRepository.findById(id).orElseThrow();
+    }
+
+    @GetMapping
+    Page<ShortenerUser> getUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
     @GetMapping("{id}/confirm/{token}")
