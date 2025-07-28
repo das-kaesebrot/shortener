c6ec554e5d61d7f5f4779d76cfe6904da8223730
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-07-28 17:02:39 +0200

    add auth request/response model

---
+++ src/main/java/eu/kaesebrot/dev/shortener/model/AuthRequest.java
@@ -0,0 +1,40 @@
+package eu.kaesebrot.dev.shortener.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+
+public class AuthRequest implements Serializable {
+    @NotNull
+    @JsonProperty("username")
+    private String username;
+
+    @NotNull
+    @JsonProperty("password")
+    private String password;
+
+    public AuthRequest() {}
+
+    public AuthRequest(String username, String password) {
+        this();
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/eu/kaesebrot/dev/shortener/model/AuthResponse.java
@@ -0,0 +1,52 @@
+package eu.kaesebrot.dev.shortener.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+
+public class AuthResponse implements Serializable {
+    @NotNull
+    @JsonProperty("jwt")
+    private String jwt;
+
+    @NotNull
+    @JsonProperty("principal_name")
+    private String principalName;
+
+    @NotNull
+    @JsonProperty("expires_at")
+    private Long expiresAt;
+
+    public AuthResponse() {}
+
+    public AuthResponse(String jwt, String principalName, Long expiresAt) {
+        this.jwt = jwt;
+        this.principalName = principalName;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getPrincipalName() {
+        return principalName;
+    }
+
+    public void setPrincipalName(String principalName) {
+        this.principalName = principalName;
+    }
+
+    public Long getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Long expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
