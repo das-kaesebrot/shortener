bc85c496f4e2b1c3154af4bb54f4da509a2be1a9
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-08-04 15:23:12 +0200

    feature: use ResponseEntity

--- src/main/java/eu/kaesebrot/dev/shortener/api/AuthController.java
+++ src/main/java/eu/kaesebrot/dev/shortener/api/AuthController.java
@@ -4,26 +4,23 @@ import java.util.UUID;
 
 import eu.kaesebrot.dev.shortener.model.*;
 import eu.kaesebrot.dev.shortener.service.AuthService;
-import eu.kaesebrot.dev.shortener.service.AuthUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import eu.kaesebrot.dev.shortener.repository.ShortenerUserRepository;
+import eu.kaesebrot.dev.shortener.repository.AuthUserRepository;
 import eu.kaesebrot.dev.shortener.service.EmailConfirmationTokenService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -50,39 +47,39 @@ public class AuthController {
         
         confirmationTokenService.generateAndSendConfirmationTokenToUser(user, URI.create(request.getRequestURL().toString()), String.format("api/v1/shortener/users/%s/confirm", user.getId().toString()));
 
-        return user;
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("users/{id}")
-    AuthUser getUser(@PathVariable UUID id) {
-        return userRepository.findById(id).orElseThrow();
+    public ResponseEntity<AuthUser> getUser(@PathVariable UUID id) {
+        return ResponseEntity.ok(userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "The user could not be found")));
     }
 
     @GetMapping("users")
-    Page<AuthUser> getUsers(Pageable pageable) {
-        return userRepository.findAll(pageable);
+    public ResponseEntity<Page<AuthUser>> getUsers(Pageable pageable) {
+        return ResponseEntity.ok(userRepository.findAll(pageable));
     }
 
     @GetMapping("users/{id}/confirm/{token}")
-    AuthUser confirmUserAccount(@PathVariable("id") UUID id, @PathVariable("token") String rawToken) {
-        AuthUser user = userRepository.findById(id).orElseThrow();
+    public ResponseEntity<AuthUser> confirmUserAccount(@PathVariable("id") UUID id, @PathVariable("token") String rawToken) {
+        AuthUser user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "The user could not be found"));
         confirmationTokenService.redeemToken(user, rawToken);
 
-        return userRepository.findById(id).orElseThrow();
+        return ResponseEntity.ok(userRepository.findById(id).get());
     }
 
     @PostMapping("login")
-    AuthResponseInitial getJwt(@Valid @RequestBody AuthRequestInitial request) {
+    public ResponseEntity<AuthResponseInitial> getJwt(@Valid @RequestBody AuthRequestInitial request) {
         AuthResponseBase responseBase = authService.authenticate(request);
 
         // TODO
         String rawRefreshToken = "token";
         passwordEncoder.encode(rawRefreshToken);
-        return new AuthResponseInitial(responseBase, rawRefreshToken);
+        return ResponseEntity.ok(new AuthResponseInitial(responseBase, rawRefreshToken));
     }
 
     @PostMapping("refresh")
-    AuthResponseRefresh refreshJwt(@Valid @RequestBody AuthRequestRefresh request) {
-        return null;
+    public ResponseEntity<AuthResponseRefresh> refreshJwt(@Valid @RequestBody AuthRequestRefresh request) {
+        return ResponseEntity.ok(null);
     }
 }
