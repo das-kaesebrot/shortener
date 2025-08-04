f6b2ff7f64043f09d198937fde0670e4b29a10d5
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-08-04 16:20:28 +0200

    add RefreshTokenRepository and -Service

---
+++ src/main/java/eu/kaesebrot/dev/shortener/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package eu.kaesebrot.dev.shortener.repository;
+
+import eu.kaesebrot.dev.shortener.model.AuthUser;
+import eu.kaesebrot.dev.shortener.model.RefreshToken;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.List;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    List<RefreshToken> findByUserAndExpiresAtAfter(AuthUser user, Instant expiresAtAfter);
+    List<RefreshToken> findByUserUsernameAndExpiresAtAfter(@NotBlank String username, Instant expiresAt);
+    long deleteByExpiresAtBefore(Instant expiresAt);
+}
---
+++ src/main/java/eu/kaesebrot/dev/shortener/service/RefreshTokenServiceImpl.java
@@ -0,0 +1,81 @@
+package eu.kaesebrot.dev.shortener.service;
+
+import eu.kaesebrot.dev.shortener.model.AuthUser;
+import eu.kaesebrot.dev.shortener.model.RefreshToken;
+import eu.kaesebrot.dev.shortener.repository.AuthUserRepository;
+import eu.kaesebrot.dev.shortener.repository.RefreshTokenRepository;
+import eu.kaesebrot.dev.shortener.utils.RandomStringGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+    private final RandomStringGenerator randomStringGenerator;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final AuthUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public String generateRefreshTokenForUser(AuthUser user, Instant expiresAt) {
+        String rawRefreshToken = randomStringGenerator.generateHexToken();
+        String encodedRefreshToken = passwordEncoder.encode(rawRefreshToken);
+
+        RefreshToken refreshToken = new RefreshToken(user, encodedRefreshToken, expiresAt);
+        refreshTokenRepository.save(refreshToken);
+
+        return rawRefreshToken;
+    }
+
+    @Override
+    public String generateRefreshTokenForUser(UUID userId, Instant expiresAt) {
+        AuthUser user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "The user could not be found"));
+        return generateRefreshTokenForUser(user, expiresAt);
+    }
+
+    @Override
+    public String generateRefreshTokenForUser(String username, Instant expiresAt) {
+        AuthUser user = userRepository.findByUsername(username).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "The user could not be found"));
+        return generateRefreshTokenForUser(user, expiresAt);
+    }
+
+    @Override
+    public boolean validateRefreshToken(String rawRefreshToken, AuthUser user) {
+        Instant now = Instant.now();
+
+        for (var token : refreshTokenRepository.findByUserAndExpiresAtAfter(user, now)) {
+            if (passwordEncoder.matches(rawRefreshToken, token.getRefreshTokenHash())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean validateRefreshToken(String rawRefreshToken, String username) {
+        Instant now = Instant.now();
+
+        for (var token : refreshTokenRepository.findByUserUsernameAndExpiresAtAfter(username, now)) {
+            if (passwordEncoder.matches(rawRefreshToken, token.getRefreshTokenHash())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public long deleteExpiredRefreshTokens() {
+        return refreshTokenRepository.deleteByExpiresAtBefore(Instant.now());
+    }
+}
