267b46a3f99baa69ed251c9a59c09a05681b523a
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-08-04 12:43:38 +0200

    feature: reduce boilerplate using lombok annotations

--- src/main/java/eu/kaesebrot/dev/shortener/properties/DatabaseProperties.java
+++ src/main/java/eu/kaesebrot/dev/shortener/properties/DatabaseProperties.java
@@ -1,14 +1,23 @@
 package eu.kaesebrot.dev.shortener.properties;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "shortener.db")
+@Getter
+@Setter
 public class DatabaseProperties {
-
+    @Getter(AccessLevel.NONE)
     private final String DEFAULT_URL = "jdbc:sqlite:shortener.db";
+
+    @Getter(AccessLevel.NONE)
     private final String DEFAULT_DRIVER_CLASS_NAME = "org.sqlite.JDBC";
+
+    @Getter(AccessLevel.NONE)
     private final String DEFAULT_DATABASE_PLATFORM = "org.hibernate.community.dialect.SQLiteDialect";
 
     private String url = DEFAULT_URL;
@@ -16,45 +25,4 @@ public class DatabaseProperties {
     private String databasePlatform = DEFAULT_DATABASE_PLATFORM;
     private String username;
     private String password;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getDriverClassName() {
-        return driverClassName;
-    }
-
-    public void setDriverClassName(String driverClassName) {
-        this.driverClassName = driverClassName;
-    }
-
-    public String getDatabasePlatform() {
-        return databasePlatform;
-    }
-
-    public void setDatabasePlatform(String databasePlatform) {
-        this.databasePlatform = databasePlatform;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
