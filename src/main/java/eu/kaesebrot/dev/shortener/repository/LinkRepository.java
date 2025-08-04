7246ecbcc46d05cbf7531110a94518a76249249f
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-08-04 15:21:12 +0200

    feature: add method for finding links by owner id

--- src/main/java/eu/kaesebrot/dev/shortener/repository/LinkRepository.java
+++ src/main/java/eu/kaesebrot/dev/shortener/repository/LinkRepository.java
@@ -2,9 +2,14 @@ package eu.kaesebrot.dev.shortener.repository;
 
 import eu.kaesebrot.dev.shortener.model.Link;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface LinkRepository extends JpaRepository<Link, String> {
+    Page<Link> findLinksByOwnerId(UUID ownerId, Pageable pageable);
 }
