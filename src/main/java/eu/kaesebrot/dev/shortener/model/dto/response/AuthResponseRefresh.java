593b30d5eb42a0536728f94fc49e292a92509c43
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-08-04 20:54:34 +0200

    fix: add override constructor for inheriting class

--- src/main/java/eu/kaesebrot/dev/shortener/model/dto/response/AuthResponseRefresh.java
+++ src/main/java/eu/kaesebrot/dev/shortener/model/dto/response/AuthResponseRefresh.java
@@ -1,4 +1,7 @@
 package eu.kaesebrot.dev.shortener.model.dto.response;
 
 public class AuthResponseRefresh extends AuthResponseBase {
+    public AuthResponseRefresh(String jwt, String username, Long expiresAt) {
+        super(jwt, username, expiresAt);
+    }
 }
